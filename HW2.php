bc29d516882953177e90c4e1e6f2f63ed05fc1b1
65160366 <152172480+65160366@users.noreply.github.com>
2023-12-21 13:50:44 +0700

    Update HW2.php

--- HW2.php
+++ HW2.php
@@ -7,24 +7,32 @@
 <body>
     <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
+    <div class = "GG">
     <table>
         <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
+            <?php
+            for($i = 1; $i <= 24 ;$i++){
+                echo "<tr>";
+                echo "<td> $multi_x X $i = ".$multi_x*$i."</td>";
+                echo "</tr>";
+            }
+            ?>
         </tbody>
     </table>
     <style>
         table {
-            width: 150px;
-            height: 265px;
+            width: 250px;
+            height: 300px;
+            background-color : pink;
+        }
+        div.GG{
+            height: 300px;
+            width: 250px;
+            overflow-y : scroll;
+        }
+        h1{
+            text-shadow: 3px 3px yellow;
         }
     </style>
 </body>
-
 </html>
